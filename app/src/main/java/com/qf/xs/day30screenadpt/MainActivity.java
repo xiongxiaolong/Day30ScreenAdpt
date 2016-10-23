0c02e253c4a0fa08acad4b64cd1281c263434a3b
xiongxiaolong <1150278244@qq.com>
2016-10-23 17:01:15 +0800

    添加了Log

--- app/src/main/java/com/qf/xs/day30screenadpt/MainActivity.java
+++ app/src/main/java/com/qf/xs/day30screenadpt/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
+        Log.e("print","改变");
+        //测试Github代码
 
     }
 
